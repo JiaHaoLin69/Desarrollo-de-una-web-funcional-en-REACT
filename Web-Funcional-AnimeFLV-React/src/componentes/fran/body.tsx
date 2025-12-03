24bb61c55e8829394da68406d231c05016b4f496
francisco <FranciscoParraCaparros2@hotmail.com>
2025-12-03 09:22:44 +0100

    Tamaño de las cartas

--- Web-Funcional-AnimeFLV-React/src/componentes/fran/body.tsx
+++ Web-Funcional-AnimeFLV-React/src/componentes/fran/body.tsx
@@ -1,8 +1,7 @@
 import { Container, Row, Col } from 'react-bootstrap';
-import './body.css'; // Importamos los estilos que crearemos abajo
+import './body.css'; 
 import { FaPlay } from "react-icons/fa";
 
-// Definimos cómo es la información de un episodio
 interface AnimeEpisode {
   id: number;
   title: string;
@@ -10,7 +9,6 @@ interface AnimeEpisode {
   image: string;
 }
 
-// Datos de prueba (simulando lo que vendría de una API)
 const episodios: AnimeEpisode[] = [
   { id: 1, title: "Akame Ga Kill", episode: 1, image: "https://imagenes.hobbyconsolas.com/files/image_640_360/uploads/imagenes/2023/04/25/6900bba05c3c5.jpeg" },
   { id: 2, title: "Solo Leveling", episode: 2, image: "https://wallpapers.com/images/hd/solo-leveling-background-c93l8rn7l59fhvg9.jpg" },
@@ -33,33 +31,21 @@ function GridAnime() {
   return (
     <Container fluid className="mt-4">
       <h2 className="mb-4 text-secondary">Últimos episodios</h2>
-      
-      <Row>
+      <Row className="mb-5">
         {episodios.map((anime) => (
-          // Responsive: 1 columna en móvil (xs), 2 en tablet (md), 4 en PC (lg)
-          <Col xs={12} md={6} lg={3} className="mb-4" key={anime.id}>
-            
+          <Col xs={12} md={4} lg={2} className="mb-4" key={anime.id}>
             <div className="anime-card">
-              {/* Imagen de fondo */}
               <img src={anime.image} alt={anime.title} className="anime-image" />
-              
-              {/* Icono de Play (esquina superior derecha) */}
               <div className="play-icon">{<FaPlay />}</div>
-
-              {/* Contenedor de información (abajo) */}
               <div className="anime-info">
                 <span className="episode-badge">Episodio {anime.episode}</span>
                 <h3 className="anime-title">{anime.title}</h3>
               </div>
-              
-              {/* Overlay oscuro al pasar el mouse (opcional) */}
-              <div className="hover-overlay"></div>
             </div>
-
           </Col>
         ))}
       </Row>
-    </Container>
+</Container>
   );
 }
 
